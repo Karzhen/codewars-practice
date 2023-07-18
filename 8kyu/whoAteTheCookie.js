ba635715a0358b381954991ff0cd007e1d124fb5
Егор Черкашин <karzhen@MBP-Egor.N4>
2023-07-18 21:50:07 +0300

    8 kyu: Who ate the cookie?

---
+++ 8kyu/whoAteTheCookie.js
@@ -0,0 +1,16 @@
+function cookie(x){
+    let name = '';
+    if (typeof x === 'string') {
+        name = "Zach";
+    }
+    else if (typeof x === 'number') {
+        name = "Monica";
+    }
+    else {
+        name = "the dog";
+    }
+    return `Who ate the last cookie? It was ${name}!`
+}
+console.log(cookie(2731.2))
+console.log(cookie('text'))
+console.log(cookie(true))
